57d52565dcf01ded27d48fa74c7e0b717477abc7
Pavel <voroshilow_pavel@mail.ru>
2025-02-17 18:59:11 +0300

    parsing

--- src/Cli.php
+++ src/Cli.php
@@ -3,6 +3,8 @@
 namespace Hexlet\Code;
 
 use Docopt;
+use Mockery\Exception;
+
 
 class Cli
 {
@@ -16,12 +18,29 @@ class Cli
 
     public function runProgram()
     {
+        $filesPath = [];
+
         foreach ($this->params as $param) {
             if ($param === '-h') {
+                 if (count($filesPath) > 0) {
+                    throw new Exception("Ошибка ввода: сначала должны быть команды");
+                 }
                 $this->showInfo();
                 exit;
             }
+             else if (substr($param, 0, 1) !== '-') {
+                $filesPath [] = $param;
+            }
         }
+
+        $filesData = [];
+
+        $filesData[] =  $this->parsing($filesPath[0]);
+        $filesData[] =  $this->parsing($filesPath[1]);
+
+        print_r($filesData);
+
+
     }
 
     public function showInfo()
@@ -46,4 +65,26 @@ DOCOPT;
             echo $k . ': ' . json_encode($v) . PHP_EOL;
         }
     }
+
+    public function parsing($filePath):array {
+
+        $content = '';
+        $file = fopen($filePath,'r');
+        if ($file) {
+
+            $content = fread($file, filesize($filePath)); // Читаем содержимое файла
+
+            fclose($file); // Закрываем файл
+        } else {
+            echo "Невозможно открыть файл";
+            exit;
+        }
+
+
+        return json_decode($content,true);
+
+    }
+
+
+
 }
